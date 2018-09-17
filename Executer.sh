f755806639548b3426bbd3694b1324c77e6f181e
Ando ANDRIANAVALONA <rakotofiringa.ando.andrianavalona@esti.mg>
2018-09-17 15:15:04 +0300

    Modification de Executer pour ajouter utilisateur parmis les sudoers

--- Executer.sh
+++ Executer.sh
@@ -2,6 +2,10 @@
 cp scripts/*.sh /home/dev
 echo "Fichier copier"
 sudo adduser dev
-
+sudo usermod -aG sudo dev
+sudo chown dev /home/dev/*.gz
+sudo chown dev /home/dev/*.sh
+sudo chmod 755 /home/dev/*.gz
+sudo chmod 755 /home/dev/*.sh
 echo "Veuillez executer le fichier Installer.sh avec < sudo bash Installer.sh >"
 su - dev
