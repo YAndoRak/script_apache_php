21dc3dc75b6601d7127392ff710a97f3a5f2015f
Ando ANDRIANAVALONA <rakotofiringa.ando.andrianavalona@esti.mg>
2018-09-17 15:08:57 +0300

    Ajout script pour installer les prerequis de CentOS

---
+++ centos_prerequis.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Installation des prerequis pour CentOS"
+
+sudo yum install update
+sudo yum install -y gcc patch byacc wget make bison binutils automake flex libt$
+echo "Installation terminer"
